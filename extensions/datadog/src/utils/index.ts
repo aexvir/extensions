f838c12f3e105b8f4356e04900d34f52fda24782
Alex <alexviscreanu@gmail.com>
2022-03-16 01:31:52 +0100

    feat(datadog): add action to mark resources as favorites

--- extensions/datadog/src/utils/index.ts
+++ extensions/datadog/src/utils/index.ts
@@ -3,3 +3,28 @@ import { getPreferenceValues } from "@raycast/api";
 export const linkDomain = () => {
   return getPreferenceValues()["domain"] || "app." + getPreferenceValues()["server"];
 };
+
+interface Identifiable {
+  id: number;
+}
+
+export function moveBetween(from: Array<Identifiable>, to: Array<Identifiable>, id: number): void {
+  from.forEach((el, idx) => {
+    if (el.id === id) {
+      to.push(el);
+      from.splice(idx, 1);
+      return;
+    }
+  });
+}
+
+export function filterOut(from: Array<Identifiable>, items: Array<Identifiable>): void {
+  items.forEach(item => {
+    from.forEach((el, idx) => {
+      if (el.id === item.id) {
+        from.splice(idx, 1);
+        return; // each item is assumed to be only once in from, so return as soon as found
+      }
+    });
+  });
+}
